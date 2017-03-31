2d01758941b019658ee5b3598453272f6de3b70b
Alejandro Medrano <alejandro.medrano@spirent.com>
2017-03-31 12:11:33 -0700

    dumpGoroutineStacks not implemented on Windows

---
+++ dumpstacks_windows.go
@@ -0,0 +1,9 @@
+package luddite
+
+import (
+	log "github.com/SpirentOrion/logrus"
+)
+
+func dumpGoroutineStacks(logger *log.Logger) {
+	logger.Debug("*** goroutine dump not implemened on windows yet***")
+}
